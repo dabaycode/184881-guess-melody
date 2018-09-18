f159b790dced448484dc8323abfb51f889511498
Dmitry Bayko <dbayko@bk.ru>
2018-09-18 21:21:15 +1000

    Bug fix

--- js/views/artist-view.js
+++ js/views/artist-view.js
@@ -1,7 +1,6 @@
 import AbstractView from '../views/abstract-view';
 import {playerWorker, stopMusic} from '../player';
 import HeaderView from '../views/header-view';
-import ServerWorker from '../server-worker';
 
 const DEBUG = new URLSearchParams(location.search).has(`debug`);
 const DEBUG_STYLE = `style="outline: 2px solid #FF9749; outline-offset: 2px; box-sizing: border-box;"`;
